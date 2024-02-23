f6dd26ee90001dae20392b2576cb8f040b0e1aba
Swayam Gupta <77153230+swayamg20@users.noreply.github.com>
2024-02-23 21:12:08 +0530

    final added

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,35 +1,45 @@
-import { Box, Button, Flex, Image } from "@chakra-ui/react";
+import { Avatar, Box, Button, Flex, Image } from "@chakra-ui/react";
 import React from "react";
 
 export default function SideBar() {
   return (
     <>
-      <Flex flexDir={"column"} p={1} alignItems={"center"}>
-        <Box p={2} mt={3}>
-          <Image src="L2493.svg" />
-        </Box>
-        <Box p={2}>
-          <Button bg={"transparent"}>
-            {" "}
-            <Image src="Icon1.svg" mt={3} mb={3} />
-          </Button>
-        </Box>
-        <Box p={2}>
-          <Button bg={"transparent"}>
-            <Image src="Icon2.svg" mt={3} mb={3} />
-          </Button>
-        </Box>
-        <Box p={2}>
-          <Button bg={"transparent"}>
-            <Image src="Icon3.svg" mt={3} mb={3} />
-          </Button>
-        </Box>
-        <Box p={2}>
-          <Button bg={"transparent"}>
-            {" "}
-            <Image src="Icon4.svg" mt={3} mb={3} />
-          </Button>
-        </Box>
+      <Flex
+        flexDir={"column"}
+        p={1}
+        justifyContent={"space-between"}
+        alignItems={"center"}
+      >
+        <Flex flexDir={"column"} alignItems={"center"}>
+          <Box p={2} mt={3}>
+            <Image src="L2493.svg" />
+          </Box>
+          <Box p={2}>
+            <Button bg={"transparent"}>
+              {" "}
+              <Image src="Icon1.svg" mt={3} mb={3} />
+            </Button>
+          </Box>
+          <Box p={2}>
+            <Button bg={"transparent"}>
+              <Image src="Icon2.svg" mt={3} mb={3} />
+            </Button>
+          </Box>
+          <Box p={2}>
+            <Button bg={"transparent"}>
+              <Image src="Icon3.svg" mt={3} mb={3} />
+            </Button>
+          </Box>
+          <Box p={2}>
+            <Button bg={"transparent"}>
+              {" "}
+              <Image src="Icon4.svg" mt={3} mb={3} />
+            </Button>
+          </Box>
+        </Flex>
+        <Flex>
+          <Avatar src="https://i.pravatar.cc/150?img=26" />
+        </Flex>
       </Flex>
     </>
   );
--- src/components/WidgetModal.tsx
+++ src/components/WidgetModal.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Modal,
   ModalOverlay,
@@ -20,19 +20,34 @@ import {
   Divider,
   Box,
 } from "@chakra-ui/react";
+import TableWidget from "./widgets/Table";
+import { BsThreeDots } from "react-icons/bs";
 
 interface Props {
   isOpen: boolean;
   onClose: () => void;
 }
 
+const dataTable = {
+  headers: ["PRODUCT", "Q1-23", "Q2-23"],
+  rows: [
+    ["Reusable", 30, "4%"],
+    ["Natural", 25, "4%"],
+    ["Compost", 35, "4%"],
+    ["Reusable Stuff", 35, "4%"],
+  ],
+  total: ["Total", "4%", "4%"],
+};
+
 const WidgetModal: React.FC<Props> = ({ isOpen, onClose }) => {
+  const [backgroundColor, setBackgroundColor] = useState<string>("transparent");
+
   return (
     <Modal size={"5xl"} isOpen={isOpen} onClose={onClose} isCentered>
       <ModalOverlay />
       <ModalContent
         overflow={"hidden"}
-        height={"fit-content"}
+        height={"700px"}
         width={"70vw"}
         borderRadius="15px"
       >
@@ -82,136 +97,350 @@ const WidgetModal: React.FC<Props> = ({ isOpen, onClose }) => {
           </Flex>
         </Flex>
 
-        <ModalBody mb={"5vh"}>
+        <ModalBody mb={"5vh"} height={"fit-content"}>
           <Flex mt={"-10vh"} flexDir={"column"}>
             <Divider />
-            <Flex flexDir={"row"} height={"40vh"}>
-              <Flex mt={"2vh"} width={"60%"} borderWidth={"1px"}>
-                BG
-              </Flex>
-              <Flex mt={"2vh"} width={"40%"} ml={3} flexDir={"column"}>
-                <Text
-                  mb={3}
-                  fontSize={"13px"}
-                  fontWeight={"500"}
-                  color="rgba(43, 43, 43, 0.35)"
-                >
-                  COMPONENTS
-                </Text>
-                <Box
-                  mb={3}
-                  p={3}
-                  height={"9vh"}
-                  borderRadius={"8px"}
-                  borderColor={"rgba(228, 228, 231, 1)"}
-                  borderWidth={"1px"}
-                  style={{
-                    filter: "drop-shadow(0px 0px 5px rgba(0, 0, 0, 0.25))",
-                  }}
-                  _hover={{
-                    borderColor: "rgba(94, 90, 219, 1)",
-                    transition: "0.3s",
-                  }}
-                >
-                  <Text
-                    fontSize={"16px"}
-                    fontWeight={"500"}
-                    color="rgba(88, 88, 88, 1)"
-                  >
-                    Data
-                  </Text>
-                  <Text
-                    fontSize={"12.8px"}
-                    fontWeight={"400"}
-                    color="rgba(136, 136, 145, 1)"
-                  >
-                    Random Description
+            <Flex flexDir={"row"} height={"500px"}>
+              <Flex
+                backgroundImage="url('modalBg.png')"
+                backgroundSize="cover"
+                backgroundPosition="center"
+                mt={"2vh"}
+                width={"60%"}
+                height={"100%"}
+                borderWidth={"1px"}
+                justifyContent="center"
+                alignItems="center"
+                borderRadius={"5px"}
+                position="relative"
+              >
+                <Box position="absolute" top="0" left="0" mt={3} ml={3}>
+                  <Text fontWeight={"600"} fontSize={"18px"} color={"#5E5ADB"}>
+                    {" "}
+                    1 X 1
                   </Text>
                 </Box>
+
+                <Box position="absolute" top="0" right="0" mt={3} mr={3}>
+                  <Flex
+                    bg={"#FFF"}
+                    width={"8vw"}
+                    height={"4vh"}
+                    alignItems={"center"}
+                    flexDir={"row"}
+                    justifyContent={"space-between"}
+                    p={2}
+                    border="1px solid #E1E1E1 "
+                  >
+                    <Box
+                      height={"2.5vh"}
+                      bg="rgba(237, 237, 237, 0.2)"
+                      border="1px solid #E1E1E1"
+                      width={"3vw"}
+                      //   ml={1}
+                    >
+                      {" "}
+                      <Text
+                        fontWeight={"400"}
+                        fontSize={"13px"}
+                        color={"rgba(123, 123, 123, 1)"}
+                      >
+                        100%
+                      </Text>
+                    </Box>
+                    <Image src="zoom-in.svg" /> <Image src="divider.svg" />{" "}
+                    <Image src="zoom-out.svg" />
+                  </Flex>
+                </Box>
+
                 <Box
-                  mb={3}
-                  p={3}
-                  height={"15vh"}
-                  borderRadius={"8px"}
-                  borderColor={"rgba(228, 228, 231, 1)"}
-                  style={{
-                    filter: "drop-shadow(0px 2px 5px rgba(0, 0, 0, 0.25))",
-                  }}
-                  borderWidth={"1px"}
-                  _hover={{
-                    borderColor: "rgba(94, 90, 219, 1)",
-                    transition: "0.3s",
-                  }}
+                  position="absolute"
+                  bottom="0"
+                  textAlign="center"
+                  width="100%"
                 >
-                  <Text
-                    fontSize={"16px"}
-                    fontWeight={"500"}
-                    color="rgba(88, 88, 88, 1)"
+                  <Circle
+                    color="rgba(40, 40, 40, 1)"
+                    selectedColor={backgroundColor}
+                    onClick={() => setBackgroundColor("rgba(40, 40, 40, 1)")}
+                  />
+                  <Circle
+                    color="rgba(255, 255, 255, 1) "
+                    selectedColor={backgroundColor}
+                    onClick={() => setBackgroundColor("#FFF")}
+                  />
+                  <Circle
+                    color="rgba(94, 90, 219, 1)"
+                    selectedColor={backgroundColor}
+                    onClick={() => setBackgroundColor("rgba(94, 90, 219, 1)")}
+                  />
+                </Box>
+
+                <Box
+                  width="12vw"
+                  mb={"2vh"}
+                  height="22vh"
+                  bg={backgroundColor}
+                  textAlign="center"
+                  borderWidth={"1px"}
+                  borderRadius={"8px"}
+                  transition="background-color 0.3s ease"
+                  onClick={() => setBackgroundColor("transparent")}
+                >
+                  <Box
+                    mt={2}
+                    height={"3vh"}
+                    bg={backgroundColor}
+                    style={{
+                      borderColor:
+                        backgroundColor === "#FFF"
+                          ? "#E1E1E1"
+                          : backgroundColor === "#5E5ADB"
+                          ? "#9290DE"
+                          : "#727272",
+                    }}
+                    borderBottom={"1px"}
                   >
-                    Graph
-                  </Text>
-                  <Text
-                    fontSize={"12.8px"}
-                    fontWeight={"400"}
-                    color="rgba(136, 136, 145, 1)"
-                  >
-                    Random Description
-                  </Text>
-                  <Box mt={1} width={"13vw"} bg={"rgba(244, 244, 245, 1)"}>
-                    <Button width={"4vw"} height={"3vh"} bg={"transparent"} mr={1}>
-                      <Image src="Complete.svg" />
-                    </Button>
-                    <Button width={"4vw"} height={"3vh"} bg={"transparent"} mr={1}>
-                      <Image src="Complete.svg" />
-                    </Button>
-                    <Button width={"4vw"} height={"3vh"} bg={"#fff"} mr={1}>
-                      <Image src="Complete.svg" />
-                    </Button>
+                    <Flex
+                      fontSize={"11px"}
+                      alignItems={"center"}
+                      justifyContent={"space-between"}
+                    >
+                      <Flex>
+                        <Flex>
+                          <Box
+                            pr={3}
+                            pl={2}
+                            m={0}
+                            bg="transparent"
+                            h="3vh"
+                            borderBottom={"2px"}
+                            style={{
+                              borderColor:
+                                backgroundColor === "#FFF"
+                                  ? "#5E5ADB"
+                                  : backgroundColor === "#5E5ADB"
+                                  ? "#FFF"
+                                  : "#FFF",
+                            }}
+                          >
+                            7d
+                          </Box>
+                          <Box pr={3} pl={2} m={0} bg="transparent" h="3vh">
+                            14d
+                          </Box>
+                          <Box pr={3} pl={2} m={0} bg="transparent" h="3vh">
+                            30d
+                          </Box>
+                        </Flex>
+                      </Flex>
+                      <Flex mr={3} mt={0} p={0}>
+                        <BsThreeDots
+                          size="3vh"
+                          style={{
+                            color:
+                              backgroundColor === "#FFF"
+                                ? "#D9D9D9"
+                                : backgroundColor === "#5E5ADB"
+                                ? "#EAEAEA"
+                                : "#ECECEC",
+                            padding: "0",
+                          }}
+                        />
+                      </Flex>
+                    </Flex>
                   </Box>
+                  <TableWidget bgColor={backgroundColor} data={dataTable} />
                 </Box>
-                <Box
-                  mb={3}
-                  p={3}
-                  height={"9vh"}
-                  borderRadius={"8px"}
-                  borderColor={"rgba(228, 228, 231, 1)"}
-                  style={{
-                    filter: "drop-shadow(0px 2px 5px rgba(0, 0, 0, 0.25))",
-                  }}
-                  borderWidth={"1px"}
-                  _hover={{
-                    borderColor: "rgba(94, 90, 219, 1)",
-                    transition: "0.3s",
-                  }}
-                >
+              </Flex>
+              <Flex
+                mt={"2vh"}
+                width={"40%"}
+                ml={3}
+                flexDir={"column"}
+                justifyContent={"space-between"}
+              >
+                <Flex flexDir={"column"}>
                   <Text
-                    fontSize={"16px"}
+                    mb={3}
+                    fontSize={"13px"}
                     fontWeight={"500"}
-                    color="rgba(88, 88, 88, 1)"
+                    color="rgba(43, 43, 43, 0.35)"
                   >
-                    Summary
+                    COMPONENTS
                   </Text>
-                  <Text
-                    fontSize={"12.8px"}
-                    fontWeight={"400"}
-                    color="rgba(136, 136, 145, 1)"
+                  <Box
+                    mb={3}
+                    p={3}
+                    height={"9vh"}
+                    borderRadius={"8px"}
+                    borderColor={"rgba(228, 228, 231, 1)"}
+                    borderWidth={"1px"}
+                    style={{
+                      filter: "drop-shadow(0px 0px 5px rgba(0, 0, 0, 0.25))",
+                    }}
+                    _hover={{
+                      borderColor: "rgba(94, 90, 219, 1)",
+                      transition: "0.3s",
+                    }}
                   >
-                    Random Description
-                  </Text>
-                </Box>
+                    <Text
+                      fontSize={"16px"}
+                      fontWeight={"500"}
+                      color="rgba(88, 88, 88, 1)"
+                    >
+                      Data
+                    </Text>
+                    <Text
+                      fontSize={"12.8px"}
+                      fontWeight={"400"}
+                      color="rgba(136, 136, 145, 1)"
+                    >
+                      Random Description
+                    </Text>
+                  </Box>
+                  <Box
+                    mb={3}
+                    p={3}
+                    height={"15vh"}
+                    borderRadius={"8px"}
+                    borderColor={"rgba(228, 228, 231, 1)"}
+                    style={{
+                      filter: "drop-shadow(0px 2px 5px rgba(0, 0, 0, 0.25))",
+                    }}
+                    borderWidth={"1px"}
+                    _hover={{
+                      borderColor: "rgba(94, 90, 219, 1)",
+                      transition: "0.3s",
+                    }}
+                  >
+                    <Text
+                      fontSize={"16px"}
+                      fontWeight={"500"}
+                      color="rgba(88, 88, 88, 1)"
+                    >
+                      Graph
+                    </Text>
+                    <Text
+                      fontSize={"12.8px"}
+                      fontWeight={"400"}
+                      color="rgba(136, 136, 145, 1)"
+                    >
+                      Random Description
+                    </Text>
+                    <Box mt={1} width={"13vw"} bg={"rgba(244, 244, 245, 1)"}>
+                      <Button
+                        width={"4vw"}
+                        height={"3vh"}
+                        bg={"transparent"}
+                        mr={1}
+                      >
+                        <Image src="Complete.svg" />
+                      </Button>
+                      <Button
+                        width={"4vw"}
+                        height={"3vh"}
+                        bg={"transparent"}
+                        mr={1}
+                      >
+                        <Image src="Complete.svg" />
+                      </Button>
+                      <Button width={"4vw"} height={"3vh"} bg={"#fff"} mr={1}>
+                        <Image src="Complete.svg" />
+                      </Button>
+                    </Box>
+                  </Box>
+                  <Box
+                    mb={3}
+                    p={3}
+                    height={"9vh"}
+                    borderRadius={"8px"}
+                    borderColor={"rgba(228, 228, 231, 1)"}
+                    style={{
+                      filter: "drop-shadow(0px 2px 5px rgba(0, 0, 0, 0.25))",
+                    }}
+                    borderWidth={"1px"}
+                    _hover={{
+                      borderColor: "rgba(94, 90, 219, 1)",
+                      transition: "0.3s",
+                    }}
+                  >
+                    <Text
+                      fontSize={"16px"}
+                      fontWeight={"500"}
+                      color="rgba(88, 88, 88, 1)"
+                    >
+                      Summary
+                    </Text>
+                    <Text
+                      fontSize={"12.8px"}
+                      fontWeight={"400"}
+                      color="rgba(136, 136, 145, 1)"
+                    >
+                      Random Description
+                    </Text>
+                  </Box>
+                </Flex>
+                <Flex>
+                  <Flex width={"20%"}>
+                    <Button
+                      border="1px solid rgba(224, 223, 248, 1)"
+                      bg={"rgba(248, 248, 255, 1)"}
+                      width={"100%"}
+                    >
+                      <Image src="counter-clockwise-clock.svg" />
+                    </Button>
+                  </Flex>
+                  <Flex width={"40%"} ml={2} mr={2}>
+                    <Button
+                      borderWidth={"1px"}
+                      bg="#FFF"
+                      borderColor={"rgba(228, 228, 231, 1)"}
+                      onClick={onClose}
+                      width={"100%"}
+                    >
+                      <Text color="rgba(159, 159, 159, 1)" fontWeight={"500"}>
+                        {" "}
+                        Cancel
+                      </Text>
+                    </Button>
+                  </Flex>
+                  <Flex width={"40%"}>
+                    <Button
+                      color="#FFF"
+                      bg="rgba(94, 90, 219, 1)"
+                      width={"100%"}
+                    >
+                      Save
+                    </Button>
+                  </Flex>
+                </Flex>
               </Flex>
             </Flex>
           </Flex>
         </ModalBody>
-
-        <ModalFooter>
-          <Button colorScheme="blue" mr={3} onClick={onClose}>
-            Close
-          </Button>
-          <Button variant="ghost">Secondary Action</Button>
-        </ModalFooter>
       </ModalContent>
     </Modal>
+  );
+};
+const Circle: React.FC<{
+  color: string;
+  selectedColor: string;
+  onClick: () => void;
+}> = ({ color, selectedColor, onClick }) => {
+  return (
+    <Box
+      as="span"
+      display="inline-block"
+      width="20px"
+      height="20px"
+      borderRadius="50%"
+      backgroundColor={color}
+      marginX="5px"
+      cursor="pointer"
+      border={color === selectedColor ? "2px solid gray" : undefined}
+      onClick={onClick}
+    />
   );
 };
 
